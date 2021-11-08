4ffcc83ddc57802629e58640e5d0e54abf2193f9
Garrison Hess <garrison.hess@gmail.com>
2021-11-08 05:26:14 +0000

    MAE down from 14 to 2.5

--- cmudb/modeling/train.py
+++ cmudb/modeling/train.py
@@ -25,8 +25,7 @@ class OUModelTrainer:
     """
 
     def __init__(
-        self,
-        input_path,
+        self, input_path,
     ):
         self.input_path = input_path
         self.stats_map = {}
@@ -35,66 +34,51 @@ class OUModelTrainer:
         pass
 
 
-def load_data(results_dir, test_size=0.2):
-    run_dirs = sorted(os.listdir(results_dir), reverse=True)
-    print(f"Runs: {run_dirs}")
-    run_timestamp = run_dirs[0]
-    run_path = Path(results_dir) / run_timestamp
-    print(f"Taking latest run, recorded at {run_timestamp} UTC")
-
-    ou_name_to_df = dict()
-
-    for filename in os.listdir(run_path):
-        if not filename.endswith(".csv"):
-            continue
-
-        filepath = os.path.join(run_path, filename)
-        ou_name = filename.split(".")[0]
-
-        if os.stat(filepath).st_size > 0:
-            ou_name_to_df[ou_name] = pd.read_csv(filepath)
-
-    for (ou_name, ou_df) in ou_name_to_df.items():
-        print(f"OU Name: {ou_name}, ou_df shape: {ou_df.shape}")
+def load_data(experiment_dir, test_size=0.2):
 
     ou_name = "ExecIndexScan"
-    df = ou_name_to_df[ou_name]
+    result_paths = [p for p in experiment_dir.glob("**/*.csv") if ou_name in str(p)]
+    print(result_paths)
+    df = pd.concat(map(pd.read_csv, result_paths))
 
-    # drop features we don't want for now
-    features_to_drop = ["start_time", "end_time", "cpu_id", "query_id"]
-    df = df.drop(features_to_drop, axis=1)
-
-    cols_to_remove = []
+    cols_to_remove = ["start_time", "end_time", "cpu_id", "query_id"]
     for col in df.columns:
         if df[col].nunique() == 1:
             cols_to_remove.append(col)
 
     df = df.drop(cols_to_remove, axis=1)
-    # print(f"Dropped zero-variance columns: {cols_to_remove}")
-    # print(f"Num Remaining: {len(df.columns)}, Num Removed {len(cols_to_remove)}")
+    print(f"Dropped zero-variance columns: {cols_to_remove}")
+    print(f"Num Remaining: {len(df.columns)}, Num Removed {len(cols_to_remove)}")
 
-    targets = [
+    all_target_cols = [
         "cpu_cycles",
         "instructions",
         "cache_references",
         "cache_misses",
+        "ref_cpu_cycles",
+        "network_bytes_read",
+        "network_bytes_written",
+        "disk_bytes_read",
+        "disk_bytes_written",
+        "memory_bytes",
         "elapsed_us",
     ]
-    features = [col for col in df.columns if col not in targets]
+    feat_cols = [col for col in df.columns if col not in all_target_cols]
+    target_cols = [col for col in df.columns if col in all_target_cols]
 
-    print(f"OU Name: {ou_name}, Features: {features}")
-    X = df[features].values
-    y = df[targets].values
+    print(f"OU Name: {ou_name}, Features: {feat_cols}")
+    X = df[feat_cols].values
+    y = df[target_cols].values
+    print(f"X shape: {X.shape}")
+    print(f"Y shape: {y.shape}")
 
     # bypass train-test split
     if test_size < 0:
-        return X, X, y, y
+        return feat_cols, target_cols, X, X, y, y
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=test_size, random_state=42
-    )
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
 
-    return X_train, X_test, y_train, y_test
+    return feat_cols, target_cols, X_train, X_test, y_train, y_test
 
 
 if __name__ == "__main__":
@@ -103,40 +87,34 @@ if __name__ == "__main__":
     args = aparser.parse_args()
 
     # load the data
-    results_dir = Path.home() / "postgres/cmudb/tscout/results/tpcc"
-    X_train, X_test, y_train, y_test = load_data(results_dir, test_size=-1)
+    experiment_name = "2021-11-08_04-26-47"
+    experiment_dir = Path.home() / "postgres/cmudb/tscout/results/tpcc" / experiment_name
+    feat_cols, target_cols, X_train, X_test, y_train, y_test = load_data(
+        experiment_dir, test_size=0.2
+    )
+
     methods = [
         "lr",
         "rf",
         "gbm",
     ]
 
-    targets = [
-        "cpu_cycles",
-        "instructions",
-        "cache_references",
-        "cache_misses",
-        "elapsed_us",
-    ]
-
     for method in methods:
         # train the model
-        ou_model = model.Model(
-            method=method, normalize=True, log_transform=True, robust=True
-        )
+        ou_model = model.Model(method=method, normalize=True, log_transform=True, robust=True)
         ou_model.train(X_train, y_train)
 
         # predict
         y_pred = ou_model.predict(X_test)
 
         print(f"\n============= Model Summary for Model: {method} =============")
-        for target_idx, target in enumerate(targets):
+        for target_idx, target in enumerate(target_cols):
             target_pred = y_pred[:, target_idx]
             target_true = y_test[:, target_idx]
-            mse = mean_squared_error(target_pred, target_true)
-            exp_var = explained_variance_score(target_pred, target_true)
-            mae = mean_absolute_error(target_pred, target_true)
-            r2 = r2_score(target_pred, target_true)
+            mse = mean_squared_error(target_true, target_pred)
+            mae = mean_absolute_error(target_true, target_pred)
+            exp_var = explained_variance_score(target_true, target_pred)
+            r2 = r2_score(target_true, target_pred)
             print(f"===== Target: {target} =====")
             print(f"MSE: {round(mse, 2)}")
             print(f"MAE: {round(mae, 2)}")
