b239f335d7f9022a511d9038eccc72ea9d8f596d
Garrison Hess <garrison.hess@gmail.com>
2021-11-17 02:40:58 +0000

    add pg_prewarm.  turn off psql pager.  check for orphaned processes between runs.  better cleanup script

--- cmudb/modeling/runner/cleanup_run.py
+++ cmudb/modeling/runner/cleanup_run.py
@@ -3,7 +3,7 @@
 import psutil
 import shutil
 from pathlib import Path
-import sys
+import argparse
 
 LISTENER_NAMES = [
     "ExecAgg",
@@ -44,37 +44,30 @@ LISTENER_NAMES = [
     "ExecWork",
 ]
 
-
-def shutdown():
-    # Shutdown postgres, tscout, and benchbase
-    print("Shutting down PG process and closing logfile")
-    for proc in psutil.process_iter(["pid", "name", "username"]):
-        proc_name = proc.info["name"].lower()
-        if proc_name.startswith("postgres") or proc_name.startswith("tscout"):
-            try:
-                proc.kill()
-            except (psutil.NoSuchProcess, psutil.ZombieProcess):
-                pass
-
-        else:
-            for listener in LISTENER_NAMES:
-                if proc_name.startswith(listener.lower()):
-                    try:
-                        proc.kill()
-                    except (psutil.NoSuchProcess, psutil.ZombieProcess):
-                        pass
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("USAGE: cleanup_run.py username")
-        exit()
-    username = sys.argv[1]
-
+def kill_tscout_and_postgres():
     print("Shutting down TScout and Postgres")
-    shutdown()
-    print("Shutdown TScout and Postgres successfully")
+    try:
+        for proc in psutil.process_iter(["name"]):
+            proc_name = proc.info["name"].lower()
 
+            if "postgres" in proc_name:
+                try:
+                    proc.kill()
+                except (psutil.NoSuchProcess, psutil.ZombieProcess):
+                    pass
+            elif "tscout" in proc_name or any(
+                [listener_name.lower() in proc_name for listener_name in LISTENER_NAMES]
+            ):
+                try:
+                    proc.kill()
+                except (psutil.NoSuchProcess, psutil.ZombieProcess):
+                    pass
+        print("Shutdown TScout and Postgres successfully")
+    except Exception as err:
+        print(f"Error shutting down TScout and Postgres: {err}")
+
+
+def chown_results(username):
     # change the tscout results ownership to the user who ran the benchmark
     results_dir = f"/home/{username}/postgres/cmudb/tscout/results/"
     print(f"Changing ownership of TScout results from root to user: {username}")
@@ -82,3 +75,16 @@ if __name__ == "__main__":
     for file in Path(results_dir).glob("**/*"):
         shutil.chown(file, user=username)
     print("Cleanup Complete")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run Postgres/Benchbase/TScout")
+    parser.add_argument("--username", help="Username to reassign file ownership", required=False)
+    args = parser.parse_args()
+
+    kill_tscout_and_postgres()
+
+    if args.username is not None:
+        chown_results(args.username)
+    else:
+        print("No username provided, cannot reassign result file ownership.")
--- cmudb/modeling/runner/runner.py
+++ cmudb/modeling/runner/runner.py
@@ -11,14 +11,96 @@ import psutil
 import signal
 
 
+BENCHMARK_NAMES = [
+    "tpcc",
+    "tpch",
+    "ycsb",
+    "wikipedia",
+    "voter",
+    "twitter",
+    "tatp",
+    "smallbank",
+    "sibench",
+    "seats",
+    "resourcestresser",
+    "noop",
+    "hyadapt",
+    "epinions",
+    "chbenchmark",
+    "auctionmark",
+]
+
+
+BENCHMARK_TABLES = {
+    "tpcc": [
+        "warehouse",
+        "district",
+        "customer",
+        "item",
+        "stock",
+        "oorder",
+        "history",
+        "order_line",
+        "new_order",
+    ],
+    "tatp": [
+        "subscriber",
+        "special_facility",
+        "access_info",
+        "call_forwarding",
+    ],
+    "tpch": [
+        "region",
+        "nation",
+        "customer",
+        "supplier",
+        "part",
+        "orders",
+        "partsupp",
+        "lineitem",
+    ],
+}
+
+
 def build_postgres(pg_dir):
+    """[Build Postgres (and extensions)]
+
+    Args:
+        pg_dir ([Path]): [Postgres root directory]
+    """
     os.chdir(pg_dir)
     print("Building Postgres")
     Popen(args=["./cmudb/build/configure.sh release"], shell=True).wait()
     Popen(["make clean -s"], shell=True).wait()
-    Popen(args=["make -j -s"], shell=True).wait()
-    Popen(args=["make install -j -s"], shell=True).wait()
+    Popen(args=["make -j world-bin -s"], shell=True).wait()
+    Popen(args=["make install-world-bin -j -s"], shell=True).wait()
     print("Built Postgres")
+
+
+def check_orphans():
+    # Check for TScout and Postgres processes from prior runs
+    # Note: this will throw an error if it finds *any* postgres processes,
+    # so it's not suited to run with multiple running Postgres instances.
+    pg_procs = []
+    tscout_procs = []
+
+    for proc in psutil.process_iter(["pid", "name", "username", "ppid", "create_time"]):
+        proc_name = proc.info["name"].lower()
+        if "postgres" in proc_name:
+            pg_procs.append(proc)
+
+        if "tscout" in proc_name:
+            tscout_procs.append(proc)
+
+    if len(pg_procs) > 0:
+        raise Exception(
+            f"Aborting: there are active postgres processes from previous runs: {pg_procs}"
+        )
+
+    if len(tscout_procs) > 0:
+        raise Exception(
+            f"Aborting because there are active tscout processes from previous runs: {tscout_procs}"
+        )
 
 
 # TODO: change psql commands to use psycopg2
@@ -60,33 +142,41 @@ def init_pg(pg_dir, results_dir):
         shell=True,
     ).wait()
 
+    # Turn off pager
+    Popen(
+        args=['''./build/bin/psql -d benchbase -P pager=off -c "SELECT 1;"'''],
+        shell=True,
+    ).wait()
+
     return pg_proc, pg_log_file
 
 
-def pg_prewarm(): 
-    pass
+def pg_prewarm(pg_dir, benchmark_name):
+    os.chdir(pg_dir)
+    print(f"Prewarming Postgres")
+    Popen(
+        args=['''./build/bin/psql -d benchbase -c "CREATE EXTENSION pg_prewarm"'''], shell=True
+    ).wait()
+
+    if benchmark_name not in BENCHMARK_TABLES.keys():
+        raise Exception(f"Benchmark {benchmark_name} doesn't have prewarm tables setup yet.")
+
+    for table in BENCHMARK_TABLES[benchmark_name]:
+        print(f"Prewarming table: {table}")
+        Popen(
+            args=[f"""./build/bin/psql -d benchbase -c "select * from pg_prewarm('{table}')";"""],
+            shell=True,
+        ).wait()
+
+    print(f"Prewarmed Postgres")
 
 
 def init_tscout(tscout_dir, results_dir):
     print(f"Starting TScout for {benchmark_name}, {experiment_name}, run_id: {run_id}")
     os.chdir(tscout_dir)
 
-    # TODO: check for existing tscout processes
-    # pg_pid = None
-    # for proc in psutil.process_iter(["pid", "name"]):
-    #     if proc.info["name"].lower() == "postgres":
-    #         if pg_pid is not None:
-    #             raise Exception("Found multiple postgres processes")
-    #         pg_pid = proc.info["pid"]
-
-    # if pg_pid is None:
-    #     raise Exception("Failed to find postgres process")
-
-    # TODO: verify postgres is running
     tscout_proc = Popen(
-        args=[
-            f"sudo python3 tscout.py `pgrep -ox postgres` --outdir {results_dir} &"
-        ],
+        args=[f"sudo python3 tscout.py `pgrep -ox postgres` --outdir {results_dir} &"],
         shell=True,
     )
     time.sleep(10)
@@ -95,7 +185,6 @@ def init_tscout(tscout_dir, results_dir):
 
 def build_benchbase(benchbase_dir):
     os.chdir(benchbase_dir)
-
     benchbase_snapshot_path = benchbase_dir / "target" / "benchbase-2021-SNAPSHOT.zip"
     benchbase_snapshot_dir = benchbase_dir / "benchbase-2021-SNAPSHOT"
 
@@ -107,7 +196,7 @@ def build_benchbase(benchbase_dir):
         Popen(args=[f"unzip {benchbase_snapshot_path}"], shell=True).wait()
 
 
-def init_benchbase(benchbase_dir, benchmark_name, input_cfg_path, benchbase_results_dir):
+def init_benchbase(benchbase_dir, benchmark_name, input_cfg_path, benchbase_results_dir, pg_dir):
     os.chdir(benchbase_dir)
     benchbase_snapshot_dir = benchbase_dir / "benchbase-2021-SNAPSHOT"
     if not os.path.exists(benchbase_snapshot_dir):
@@ -151,12 +240,9 @@ def exec_benchbase(benchbase_dir, benchmark_name, benchbase_results_dir):
 
     # Copy benchbase results to experiment results directory
     benchbase_stats_dir = benchbase_snapshot_dir / "results"
-    print(f"Copying Benchbase results from {benchbase_stats_dir} to {benchbase_results_dir}")
-    # result_files = benchbase_results_dir.glob("**/*")
-    shutil.move(benchbase_stats_dir, benchbase_results_dir)
-    # for file in result_files:
-    #     print(f"Copying {file} to {benchbase_run_results_dir}")
-    #     shutil.copy(file, benchbase_run_results_dir)
+    print(f"Moving Benchbase results from {benchbase_stats_dir} to {benchbase_results_dir}")
+    print(f"Benchbase results dir: {benchbase_stats_dir}")
+    shutil.move(str(benchbase_stats_dir), str(benchbase_results_dir))
 
 
 def cleanup_run(runner_dir, err, message=""):
@@ -164,11 +250,12 @@ def cleanup_run(runner_dir, err, message=""):
         print(message)
 
     if err is not None:
-        print(err)
+        print(f"Error: {err}")
+
     print("Calling cleanup script")
     cleanup_script_path = runner_dir / "cleanup_run.py"
     username = psutil.Process().username()
-    Popen(args=[f"sudo python3 {cleanup_script_path} {username}"], shell=True).wait()
+    Popen(args=[f"sudo python3 {cleanup_script_path} --username {username}"], shell=True).wait()
 
 
 if __name__ == "__main__":
@@ -178,6 +265,7 @@ if __name__ == "__main__":
     parser.add_argument("--benchmark-name", default="tpcc")
     parser.add_argument("--experiment-name", default=datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
     parser.add_argument("--nruns", type=int, default=1)
+    parser.add_argument("--no-prewarm", action="store_true", default=False)
 
     args = parser.parse_args()
     build_pg = args.build_pg
@@ -185,11 +273,11 @@ if __name__ == "__main__":
     benchmark_name = args.benchmark_name
     experiment_name = args.experiment_name
     nruns = args.nruns
+    prewarm = not args.no_prewarm
     if nruns < 0 or nruns > 10:
         raise Exception("Invalid nruns: {runs}")
 
-    valid_benchmarks = ["tpcc", "tpch", "ycsb", "wikipedia", "voter", "twitter", "tatp", "smallbank", "sibench", "seats", "resourcestresser", "noop", "hyadapt", "epinions", "chbenchmark", "auctionmark"]
-    if benchmark_name not in valid_benchmarks:
+    if benchmark_name not in BENCHMARK_NAMES:
         raise ValueError(f"Invalid benchmark name: {benchmark_name}")
 
     pg_dir = Path.home() / "postgres"
@@ -201,6 +289,9 @@ if __name__ == "__main__":
     benchbase_dir = Path.home() / "benchbase"
     experiment_dir = tscout_dir / "results" / benchmark_name / experiment_name
     Path(experiment_dir).mkdir(parents=True, exist_ok=True)
+
+    # Check for orphaned processes from prior runs as they cause the runner to fail
+    check_orphans()
 
     if build_pg:
         try:
@@ -216,7 +307,9 @@ if __name__ == "__main__":
             cleanup_run(runner_dir, err, message="Error building benchbase")
             exit(1)
 
-    print(f"Running experiment: {experiment_name} with {nruns} runs and experiment output dir: {experiment_dir}")
+    print(
+        f"Running experiment: {experiment_name} with {nruns} runs and experiment output dir: {experiment_dir}"
+    )
     for run_id in range(nruns):
         print(f"Starting run {run_id}")
         results_dir = experiment_dir / str(run_id)
@@ -227,14 +320,23 @@ if __name__ == "__main__":
         try:
             pg_proc, pg_log_file = init_pg(pg_dir, results_dir)
         except Exception as err:
-            cleanup_run(runner_dir, err, message="Error initializing postgres")
+            cleanup_run(runner_dir, err, message="Error initializing Postgres")
             exit(1)
 
         try:
-            init_benchbase(benchbase_dir, benchmark_name, benchmark_cfg_path, benchbase_results_dir)
+            init_benchbase(
+                benchbase_dir, benchmark_name, benchmark_cfg_path, benchbase_results_dir, pg_dir
+            )
         except Exception as err:
-            cleanup_run(runner_dir, err, message="Error initializing benchbase")
+            cleanup_run(runner_dir, err, message="Error initializing Benchbase")
             exit(1)
+
+        if prewarm:
+            try:
+                pg_prewarm(pg_dir, benchmark_name)
+            except Exception as err:
+                cleanup_run(runner_dir, err, message="Error prewarming Postgres")
+                exit(1)
 
         try:
             tscout_proc = init_tscout(tscout_dir, results_dir)
@@ -245,9 +347,9 @@ if __name__ == "__main__":
         try:
             exec_benchbase(benchbase_dir, benchmark_name, benchbase_results_dir)
         except Exception as err:
-            cleanup_run(runner_dir, err, message="Error running benchbase")
+            cleanup_run(runner_dir, err, message="Error running Benchbase")
             exit(1)
-        
+
         time.sleep(5)
         tscout_proc.send_signal(signal.SIGINT)
         pg_proc.kill()
