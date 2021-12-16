eb53b0026fa5e4691cf8ba66adf94c79798f6d45
Garrison Hess <garrison.hess@gmail.com>
2021-12-16 20:16:09 +0000

    more plan rebuilding

--- cmudb/tscout/runner/rebuild_plans.py
+++ cmudb/tscout/runner/rebuild_plans.py
@@ -53,6 +53,7 @@ common_schema = ["query_id",
                  "plan_node_id",
                  "start_time",
                  "end_time",
+                 "elapsed_us",
                  "cpu_id",
                  "cpu_cycles",
                  "instructions",
@@ -63,8 +64,7 @@ common_schema = ["query_id",
                  "network_bytes_written",
                  "disk_bytes_read",
                  "disk_bytes_written",
-                 "memory_bytes",
-                 "elapsed_us"]
+                 "memory_bytes"]
 
 
 for i in range(len(dfs)):
@@ -73,21 +73,47 @@ for i in range(len(dfs)):
 df = pd.concat(dfs, axis=0)
 df = df.sort_values(by=["query_id", "start_time", "plan_total_cost"], axis=0)
 df["query_id"] = df["query_id"].astype(np.uint64)
-# df.to_csv("./test_ous.csv", index=False)
+df.to_csv("./test_ous.csv", index=False)
 
-# 15237359993683966,
-# 13274073564061327360
-temp = df[df["plan_node_id"] == 3]
-temp_query_id = df["query_id"].unique()[0]
+
+
+###### Sample data
+# temp = df[df["plan_node_id"] == 3]
+# temp_query_id = df["query_id"].unique()[0]
+temp_query_id = 413418709290753216
 subdf = df[df["query_id"] == temp_query_id]
-
-print(subdf[["query_id", "ou_name", "plan_total_cost", "start_time", "end_time", "plan_node_id"]])
-# print(subdf[subdf["ou_name"] == "ExecLimit"].count())
-# print(subdf[subdf["ou_name"] == "ExecIndexScan"].count())
-# print(subdf.count())
-subdf.to_csv("./some_query.csv", index=False)
+subdf = subdf[["query_id", "ou_name", "plan_total_cost", "start_time", "end_time", "elapsed_us", "plan_node_id"]]
+subdf.to_csv("./modify_table_with_indexscan.csv", index=False)
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def rollup(df):
+
+    rolled_up = df
+
+    # for each plan node
+    rolled_up[rolled_up["plan_node_id" == 0]]
+
+    # get the min start time
+
+    # get the max end time
+
+    # check plan total cost behavior
+
+    return rolled_up
