cbc9765b759ae334ddef59b76b7e04b2a7c181d3
Garrison Hess <garrison.hess@gmail.com>
2021-12-28 01:59:55 +0000

    plan stuff

---
+++ cmudb/behavior_modeling/test_extensions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+rm -rf data
+mkdir data
+./build/bin/pg_ctl initdb -D data
+cp ./cmudb/behavior_modeling/config/datagen/postgres/postgresql.conf ./data
+./build/bin/pg_ctl -D data start
+./build/bin/createdb 'test'
+./build/bin/psql -d 'test' -c "CREATE EXTENSION pg_stat_statements;"
+./build/bin/psql -d 'test' -c "EXPLAIN (VERBOSE) SELECT 1;"
+./build/bin/psql -d 'test' -c "SELECT * FROM pg_stat_statements;"
+./build/bin/pg_ctl -D data stop
